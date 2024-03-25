bf3674d781487048193e8e2e9af3884c9ab4c4b4
angeloantoni <angeloflexus27@gmail.com>
2024-03-25 20:58:52 -0300

    bingo.py atualizado

--- lista_listas/bingo.py
+++ lista_listas/bingo.py
@@ -11,11 +11,11 @@ class Bingo:
 
     def set_numBolas(self, numBolas):
         if numBolas >= 0: self._numBolas = numBolas
-        else: raise ValueError() 
+        else: raise ValueError("Digite um inteiro positivo") 
 
     def set_bolas(self, bolas):
         if bolas >= 0: self._bolas = bolas
-        else: raise ValueError()
+        else: raise ValueError("Digite um inteiro positivo")
     
     def get_numBolas(self):
         return self._numBolas 
@@ -24,8 +24,13 @@ class Bingo:
         return self._bolas 
     
     def Proximo(self):
-        sorteio_bola = random.randint(1, self._numBolas)
+        sorteio_bola = []
+        while len(sorteio_bola) < self._numBolas:
+            numero = random.randint(1, self._numBolas)
+            if numero not in sorteio_bola: 
+                sorteio_bola.append(numero)
         return sorteio_bola
+
 
     def Sorteados(self):    
         self._lista = []
@@ -34,24 +39,16 @@ class Bingo:
             if bola not in self._lista:
                 self._lista.append(bola)
         return self._lista
-
-    def Remove(self):
-        while len(self._lista) < self._numBolas: 
-            bola = self.Proximo()
-            if bola not in self._lista:
-                self._lista.append(bola)
-        return self._lista
-
+    
 class Play:
     @staticmethod
-    def main():        
+    def main():
+      num_bolas = int(input("Qual a quantidade de bolas a serem sorteadas? "))        
       play = Bingo()
-      play.set_numBolas(10)
-
-      print(play.Sorteados())
+      play.set_numBolas(num_bolas)
+      print(play.Proximo())
 
 Play.main()
-
 
 
         
